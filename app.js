5ce1f9e41c6ff66f54b3d8f04501e42a2f7bf36d
IdoGo15 <idogolan15@gmail.com>
2020-10-27 17:09:19 +0200

    Added validation for input & clearing input after submiting

--- app.js
+++ app.js
@@ -15,26 +15,36 @@ clearButton.addEventListener('click', clearTasks);
 // Add task
 function addTask (e) {  
   var newTask = document.getElementById('task').value;
-  var li = document.createElement('li');
-  li.className = 'list-group-item text-dark h4 border-light bg-light';
-  li.appendChild(document.createTextNode(newTask));
-  var cmplt = document.createElement('button');
-  cmplt.className = 'btn text-success far fa-check-circle fa-1x float-right completed';
-  var dltbtn = document.createElement('button');
-  dltbtn.className = 'btn text-danger far fa-times-circle fa-1x float-right delete';
-  li.appendChild(dltbtn);
-  li.appendChild(cmplt);
-  tasklist.appendChild(li);
-
-  //Create clear button if not Exists
-  if (clrbtn === false) {
-    clrbtn = true;
-    var clearbtn = document.createElement('button');
-    clearbtn.className = 'btn btn-md text-success border-success rounded';
-    clearbtn.appendChild(document.createTextNode('Clear Items'));
-    clearButton.appendChild(clearbtn);
+  // Check if empty
+  if(newTask === '') {
+    alert('Please enter a new task');
   }
-}
+  else {
+    var li = document.createElement('li');
+    li.className = 'list-group-item text-dark h4 border-light bg-light';
+    li.appendChild(document.createTextNode(newTask));
+    var cmplt = document.createElement('button');
+    cmplt.className = 'btn text-success far fa-check-circle fa-1x float-right completed';
+    var dltbtn = document.createElement('button');
+    dltbtn.className = 'btn text-danger far fa-times-circle fa-1x float-right delete';
+    li.appendChild(dltbtn);
+    li.appendChild(cmplt);
+    tasklist.appendChild(li);
+    document.getElementById('task').value = '';
+    
+  
+    //Create clear button if not Exists
+    if (clrbtn === false) {
+      clrbtn = true;
+      var clearbtn = document.createElement('button');
+      clearbtn.className = 'btn btn-md text-success border-success rounded';
+      clearbtn.appendChild(document.createTextNode('Clear Items'));
+      clearButton.appendChild(clearbtn);
+    }
+  }
+  }
+
+
 
 // Remove task
 function removeTask(e) {
