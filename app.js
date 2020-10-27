a9a3ca5811ece95e69e369f12a0f6707555695f2
IdoGo15 <idogolan15@gmail.com>
2020-10-27 20:29:18 +0200

    Fixed clear all issue

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var clearButton = document.getElementById('clearButton');
-var addInput = document.getElementById('submit-new');
-var tasklist = document.getElementById('list');
+let clearButton = document.getElementById('clearButton');
+let addInput = document.getElementById('submit-new');
+let tasklist = document.getElementById('list');
 const acomplisheClass = document.querySelector('.acomplishe');
 const complitedClass = document.querySelector('.complited');
 let clrbtn = false;
@@ -28,12 +28,12 @@ function addTask (e) {
     alert('Please enter a new task');
   }
   else {
-    var li = document.createElement('li');
+    let li = document.createElement('li');
     li.className = 'list-group-item text-dark h4 border-light bg-light';
     li.appendChild(document.createTextNode(newTask));
-    var cmplt = document.createElement('button');
+    let cmplt = document.createElement('button');
     cmplt.className = 'btn text-success far fa-check-circle fa-1x float-right completed';
-    var dltbtn = document.createElement('button');
+    let dltbtn = document.createElement('button');
     dltbtn.className = 'btn text-danger far fa-times-circle fa-1x float-right delete';
     li.appendChild(dltbtn);
     li.appendChild(cmplt);
@@ -46,10 +46,12 @@ function addTask (e) {
     acomplisheCount +=1;
     acomplisheClass.textContent = ' ' + acomplisheCount;
 
+    clearButton.style.display = 'block';
+    
     //Create clear button if not Exists
     if (clrbtn === false) {
       clrbtn = true;
-      var clearbtn = document.createElement('button');
+      let clearbtn = document.createElement('button');
       clearbtn.className = 'btn btn-md text-success border-success rounded';
       clearbtn.appendChild(document.createTextNode('Clear Items'));
       clearButton.appendChild(clearbtn);
@@ -68,7 +70,7 @@ function removeTask(e) {
         acomplisheCount -=1;
         acomplisheClass.textContent = ' ' + acomplisheCount;
       }
-      var li = e.target.parentElement;
+      let li = e.target.parentElement;
       tasklist.removeChild(li);
       
       if (acomplisheCount === 0 && completedCount === 0) {
